960d4be2eb9019c1b609b1f4e6692e0586d55ce5
abelardojr0 <abelardo.junior.181@gmail.com>
2025-04-09 11:35:39 -0300

    fix: conecntado com o back online

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://teixeira-motos-back.onrender.com',
 });
 let isRefreshing = false;
 let refreshSubscribers: any = [];
