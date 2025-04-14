e1d28fce6665275c70f20ac4e1ebc81d9bc099f1
abelardojr0 <abelardo.junior.181@gmail.com>
2025-04-14 16:55:17 -0300

    fix: ajustes deslogar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,19 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutesAdmin } from "./app_admin.routes";
+import { useNavigate } from "react-router-dom";
 
 function RoutesApp() {
   const user = localStorage.getItem("userId");
   const auth = useContext(AuthContext);
   const [route, setRoute] = useState<any>();
-
+  const navigate = useNavigate();
   useEffect(() => {
     if (user && auth) {
       setRoute(<AppRoutesAdmin />);
     } else {
       localStorage.clear();
+      navigate("/");
       setRoute(<AuthRoutes />);
     }
   }, [auth]);
